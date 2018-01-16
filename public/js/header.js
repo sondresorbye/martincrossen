d90d22ce4939503ddce663840663dd3cb62c09e1
Sondre Engesæter Sørbye <sonden0111@osloskolen.no>
2018-01-16 21:11:34 +0100

    fixed retarded header

--- public/js/header.js
+++ public/js/header.js
@@ -2,10 +2,31 @@ var dropDown = false;
 
 document.getElementById('menuBtn').onclick = function() {
     if (dropDown) {
-    	document.getElementById('dropDown').style.marginTop = "-328px";
-    	dropDown = false;
+        document.getElementById('dropDown').style.marginTop = "-398px";
+        dropDown = false;
     } else {
         document.getElementById('dropDown').style.marginTop = "0px";
         dropDown = true;
     }
 };
+
+var scroll_pos = 0;
+var scroll_time;
+
+$(window).scroll(function() {
+    clearTimeout(scroll_time);
+    var current_scroll = $(window).scrollTop();
+
+    if (current_scroll >= $('#header').outerHeight()) {
+        if (current_scroll <= scroll_pos) {
+            $('#header').removeClass('hidden');    
+        }
+        else {
+            $('#header').addClass('hidden');  
+        }
+    }
+
+    scroll_time = setTimeout(function() {
+        scroll_pos = $(window).scrollTop();
+    }, 100);
+});
