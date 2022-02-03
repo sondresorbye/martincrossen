f1258a9a75241ae97ffb84d516dd5e56116793e5
sondresorbye0111 <sondre@magson.no>
2022-02-03 22:21:17 +0100

    update for 2022

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("March 9, 2020 17:30").getTime();
+var countDownDate = new Date("March 21, 2022 17:30").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
